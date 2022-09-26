775b6673fdc79249d278e2c5b0061fbd0342efc1
Emo-hk <113575780+Emo-hk@users.noreply.github.com>
2022-09-26 10:36:34 -0700

    Change global setting defaults

--- Settings.cs
+++ Settings.cs
@@ -3,7 +3,7 @@
     public class GlobalSettings
     {
         public bool ShowKeyTimeline = true;
-        public bool ShowStagTimeline = false;
+        public bool ShowStagTimeline = true;
         public bool ShowDefendersCrest = true;
         public bool ShowSporeShroom = true;
         public bool ShowFragileCharms = true;
