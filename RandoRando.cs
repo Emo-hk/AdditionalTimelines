6dbbffe043d4844382012d9d0386a7d5c321ecd7
Emo-hk <113575780+Emo-hk@users.noreply.github.com>
2023-07-17 13:44:06 -0700

    Fix errors in non-rando saves

--- RandoRando.cs
+++ RandoRando.cs
@@ -1,17 +1,29 @@
-﻿namespace AdditionalTimelines
+namespace AdditionalTimelines
 {
     internal static class RandoRando
     {
         public static bool LoreTabletsOn()
         {
+            if (!RandomizerMod.RandomizerMod.IsRandoSave)
+            {
+                return true;
+            }
             return RandomizerMod.RandomizerMod.RS.GenerationSettings.PoolSettings.LoreTablets;
         }
         public static bool RancidEggsOn()
         {
+            if (!RandomizerMod.RandomizerMod.IsRandoSave)
+            {
+                return true;
+            }
             return RandomizerMod.RandomizerMod.RS.GenerationSettings.PoolSettings.RancidEggs;
         }
         public static bool CharmsOn()
         {
+            if (!RandomizerMod.RandomizerMod.IsRandoSave)
+            {
+                return true;
+            }
             return RandomizerMod.RandomizerMod.RS.GenerationSettings.PoolSettings.Charms;
         }
     }
